9d940f7397f2378d5652be5e8c38e515a60fab0c
Kushan Zaveri <kushanzaveri99@gmail.com>
2018-12-26 20:04:01 -0500

    removed unused import

--- bokeh_app/main.py
+++ bokeh_app/main.py
@@ -4,7 +4,6 @@ from bokeh.layouts import row, column
 
 from scripts.core_new import Core
 from scripts.imageview import ImageView
-from scripts.process import getImageData
 import scripts.utils as u
 
 imgView = ImageView()
