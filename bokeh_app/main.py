5c8f12909b85178666c7dd6a7f980ac698370a7d
Kushan Zaveri <kushanzaveri99@gmail.com>
2018-12-27 00:08:25 -0500

    I dont like this

--- bokeh_app/main.py
+++ bokeh_app/main.py
@@ -2,12 +2,15 @@ from bokeh.io import curdoc
 from bokeh.models.widgets import Button
 from bokeh.layouts import row, column
 
-from scripts.core_new import Core
-from scripts.imageview import ImageView
+# from scripts.core_new import Core
+# from scripts.imageview import ImageView
 
-imgView = ImageView()
-imgViewLayout = imgView.getView()
-core = Core(imgView)
-coreLayout = core.getView()
+# imgView = ImageView()
+# imgViewLayout = imgView.getView()
+# core = Core(imgView)
+# coreLayout = core.getView()
 
-curdoc().add_root(row(imgViewLayout, coreLayout))
+button = Button(label="HEROKU!!!")
+
+# curdoc().add_root(row(imgViewLayout, coreLayout))
+curdoc().add_root(button)
